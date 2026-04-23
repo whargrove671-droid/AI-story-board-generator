01c7ca97a6d3e65c43cbcb3a6172bd55f2801da2
whargrove671-droid <w.hargrove671@gmail.com>
2026-04-23 18:56:27 +0100

    chore: add @ffmpeg-installer/ffmpeg and fluent-ffmpeg to serverExternalPackages in next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  serverExternalPackages: ['@ffmpeg-installer/ffmpeg', 'fluent-ffmpeg'],
 };
 
 module.exports = nextConfig;
